054eed0039507cf49c822a50f47b3f7949988bcf
roniellysantana <ronielly.santana94@gmail.com>
2026-09-20 22:13:46 -0300

    Adiciona validações visuais ao cadastro de alunos

--- app/Views/alunos/criar.php
+++ app/Views/alunos/criar.php
@@ -7,6 +7,17 @@
         </div>
     </div>
 
+    <?php if (!empty($erros)): ?>
+        <div class="mensagem mensagem-erro">
+            <strong>⚠ Verifique os dados informados:</strong>
+            <ul>
+                <?php foreach ($erros as $erro): ?>
+                    <li><?= htmlspecialchars($erro) ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+
     <form
         class="formulario"
         method="POST"
@@ -19,6 +30,7 @@
                 type="text"
                 id="nome"
                 name="nome"
+                value="<?= htmlspecialchars($dados['nome'] ?? '') ?>"
                 required
             >
         </div>
@@ -29,6 +41,7 @@
                 type="email"
                 id="email"
                 name="email"
+                value="<?= htmlspecialchars($dados['email'] ?? '') ?>"
                 required
             >
         </div>
@@ -39,6 +52,7 @@
                 type="text"
                 id="telefone"
                 name="telefone"
+                value="<?= htmlspecialchars($dados['telefone'] ?? '') ?>"
             >
         </div>
 
@@ -48,6 +62,7 @@
                 type="text"
                 id="concurso_alvo"
                 name="concurso_alvo"
+                value="<?= htmlspecialchars($dados['concurso_alvo'] ?? '') ?>"
                 required
             >
         </div>
@@ -56,39 +71,44 @@
             <label for="meta_semanal_sessoes">
                 Meta semanal de sessões
             </label>
-
             <input
                 type="number"
                 id="meta_semanal_sessoes"
                 name="meta_semanal_sessoes"
                 min="1"
+                value="<?= htmlspecialchars($dados['meta_semanal_sessoes'] ?? '') ?>"
                 required
             >
         </div>
 
         <div class="campo">
             <label for="data_inicio">Início da mentoria</label>
-
             <input
                 type="date"
                 id="data_inicio"
                 name="data_inicio"
+                value="<?= htmlspecialchars($dados['data_inicio'] ?? '') ?>"
                 required
             >
         </div>
 
         <div class="campo">
             <label for="plano">Plano</label>
-
             <select
                 id="plano"
                 name="plano"
                 required
             >
                 <option value="">Selecione</option>
-                <option value="trimestral">Trimestral</option>
-                <option value="semestral">Semestral</option>
-                <option value="anual">Anual</option>
+                <option value="trimestral" <?= ($dados['plano'] ?? '') === 'trimestral' ? 'selected' : '' ?>>
+                    Trimestral
+                </option>
+                <option value="semestral" <?= ($dados['plano'] ?? '') === 'semestral' ? 'selected' : '' ?>>
+                    Semestral
+                </option>
+                <option value="anual" <?= ($dados['plano'] ?? '') === 'anual' ? 'selected' : '' ?>>
+                    Anual
+                </option>
             </select>
         </div>
 
