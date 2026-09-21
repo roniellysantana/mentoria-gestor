974a63818bacc57a2a78d7eeb5f3907f455c010b
roniellysantana <ronielly.santana94@gmail.com>
2026-09-20 22:13:54 -0300

    Adiciona rotas de edição atualização e exclusão

--- public/index.php
+++ public/index.php
@@ -16,9 +16,14 @@ require_once __DIR__ . '/../app/Controllers/RelatorioController.php';
 $router = new Router();
 
 $router->get('/', [HomeController::class, 'index']);
+
 $router->get('/alunos', [AlunoController::class, 'index']);
 $router->get('/alunos/criar', [AlunoController::class, 'criar']);
 $router->post('/alunos/salvar', [AlunoController::class, 'salvar']);
+$router->get('/alunos/editar', [AlunoController::class, 'editar']);
+$router->post('/alunos/atualizar', [AlunoController::class, 'atualizar']);
+$router->post('/alunos/excluir', [AlunoController::class, 'excluir']);
+
 $router->get('/concursos', [ConcursoController::class, 'index']);
 $router->get('/disciplinas', [DisciplinaController::class, 'index']);
 $router->get('/relatorios', [RelatorioController::class, 'index']);
